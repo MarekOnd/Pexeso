cc60a83858b9ed90ba2a909fa8f728cdf3e327b0
Trollak <98963711+MarekOnd@users.noreply.github.com>
2023-04-09 14:13:16 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace Pexeso
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.start = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pocetHracu = new System.Windows.Forms.ComboBox();
+            this.konec = new System.Windows.Forms.Label();
+            this.vyberPoctuHracu = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // start
+            // 
+            this.start.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.start.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.start.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.start.Font = new System.Drawing.Font("Segoe UI", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.start.Location = new System.Drawing.Point(519, 507);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(279, 120);
+            this.start.TabIndex = 0;
+            this.start.Text = "Start";
+            this.start.UseVisualStyleBackColor = false;
+            this.start.Click += new System.EventHandler(this.start_Click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Yellow;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 150F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(302, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(721, 265);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Pexeso";
+            // 
+            // pocetHracu
+            // 
+            this.pocetHracu.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pocetHracu.FormattingEnabled = true;
+            this.pocetHracu.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6"});
+            this.pocetHracu.Location = new System.Drawing.Point(593, 401);
+            this.pocetHracu.Name = "pocetHracu";
+            this.pocetHracu.Size = new System.Drawing.Size(121, 23);
+            this.pocetHracu.TabIndex = 5;
+            // 
+            // konec
+            // 
+            this.konec.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.konec.AutoSize = true;
+            this.konec.Font = new System.Drawing.Font("Segoe UI", 90F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.konec.Location = new System.Drawing.Point(370, 141);
+            this.konec.Name = "konec";
+            this.konec.Size = new System.Drawing.Size(591, 159);
+            this.konec.TabIndex = 6;
+            this.konec.Text = "Konec hry";
+            this.konec.Visible = false;
+            // 
+            // vyberPoctuHracu
+            // 
+            this.vyberPoctuHracu.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.vyberPoctuHracu.AutoSize = true;
+            this.vyberPoctuHracu.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.vyberPoctuHracu.Location = new System.Drawing.Point(593, 370);
+            this.vyberPoctuHracu.Name = "vyberPoctuHracu";
+            this.vyberPoctuHracu.Size = new System.Drawing.Size(113, 28);
+            this.vyberPoctuHracu.TabIndex = 7;
+            this.vyberPoctuHracu.Text = "Počet hráčů";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Yellow;
+            this.ClientSize = new System.Drawing.Size(1339, 752);
+            this.Controls.Add(this.vyberPoctuHracu);
+            this.Controls.Add(this.konec);
+            this.Controls.Add(this.pocetHracu);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.start);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button start;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox pocetHracu;
+        private System.Windows.Forms.Label konec;
+        private System.Windows.Forms.Label vyberPoctuHracu;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pexeso
+{
+    public partial class Form1 : Form
+    {
+        class Hrac//údaje o hráči
+        {
+            public int skore=0;
+            public Label Jmeno = new Label();
+            public Label Skore = new Label();
+            public Hrac(string jmeno,int poradi)
+            {
+                Jmeno.Text = jmeno;
+                Jmeno.Size = new Size(100, 50);
+                Jmeno.Location = new Point(100,100*poradi+100);
+                Jmeno.Font = new Font("Segoe UI",20);
+                Skore.Text = "0";
+                Skore.Size = new Size(100, 50);
+                Skore.Location = new Point(100, 100 * poradi+150);
+                Skore.Font = new Font("Segoe UI", 20);
+
+            }
+
+            public void ziskaniBodu()
+            {
+                skore++;
+                Skore.Text = (skore).ToString();
+            }
+        }
+        class Karta:Button//údaje o kartě
+        {
+            public int CisloObrazku;
+            public bool Otocena;
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private Karta[,] pexeso = new Karta[8,8];//hrací karty v poli
+        private List<Hrac> hraci = new List<Hrac>();//list hráčů
+        private int index = 0;//určuje hrajícího hráče
+        private int otocenaKarta;
+        private bool muzeOtocit=true;//určuje jestli je možné otočit karty
+        private int pocetDvojic = 32;//ukládá, kolik zbývá najít dvojic
+        private void start_Click(object sender, EventArgs e)
+        {
+            if(pocetHracu.SelectedIndex>=0)//zapne hru pouze pokud byl zvolen počet hráčů
+            {
+                (sender as Button).Hide();
+                label1.Hide();
+                pocetHracu.Hide();
+                vyberPoctuHracu.Hide();
+                Michani();
+                for (int i = 0; i <= pocetHracu.SelectedIndex; i++)//vytvoří daný počet hráčů
+                {
+                    Hrac hrac = new Hrac($"Hráč {i + 1}", i);
+                    hraci.Add(hrac);
+                    Controls.Add(hraci[i].Jmeno);
+                    Controls.Add(hraci[i].Skore);
+                }
+                hraci[index % (hraci.Count)].Jmeno.BackColor = Color.Red;//zvýrazní prvního hráče na řadě
+            }
+            else
+            {
+                MessageBox.Show("Není vybrán počet hráčů");
+            }
+            
+        }
+        private async void Michani()//vytvoří hrací karty
+        {
+            muzeOtocit = false;
+            for (int i = 0; i < 8; i++)//vytvoří karty, přidělí jim pozice a na pozadí nastaví obrázek rubu karet
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    await Task.Delay(40);//způsobí, že se karty objevují postupně
+                    pexeso[i, j] = new Karta();
+                    pexeso[i, j].Location = new Point(120 * j + 300, 120 * i + 100);
+                    pexeso[i, j].Size = new Size(110, 110);
+                    pexeso[i, j].BackgroundImage = Image.FromFile("Obrazky/PexesoRub.png");
+                    var pozadi = new Bitmap(pexeso[i, j].BackgroundImage, new Size(pexeso[i, j].Width, pexeso[i, j].Height));
+                    pexeso[i, j].BackgroundImage = pozadi;
+                    pexeso[i, j].CisloObrazku = 0;
+                    pexeso[i, j].Click += new System.EventHandler(pexeso_Click);
+                    
+                    Controls.Add(pexeso[i, j]);
+                }
+            }
+
+            Random rnd = new Random();
+            int x, y, r, g, b;
+            for (int i=1;i<=32;i++)//dvojicím karet přidelí stejná čísla (odkazují na obrázky) a stejná náhodně barevná pozadí 
+            {
+                r = rnd.Next(0, 256);
+                g = rnd.Next(0, 256);
+                b = rnd.Next(0, 256);
+                for (int j=0;j<2;j++)
+                {
+                    do
+                    {
+                        x = rnd.Next(0, 8);
+                        y = rnd.Next(0, 8);
+                    } while (pexeso[x, y].CisloObrazku != 0);
+                    pexeso[x, y].CisloObrazku = i;
+                    pexeso[x,y].BackColor = Color.FromArgb(r, g, b);
+                }
+            }
+            muzeOtocit = true;
+        }
+        
+        private async void pexeso_Click(object sender, EventArgs e)//otáčení karet
+        {
+            if(!(sender as Karta).Otocena && muzeOtocit)//otočit lze pouce neotočenou kartu v době, kdy je možné otáčet karty
+            {
+                (sender as Karta).BackgroundImage = Image.FromFile($"Obrazky/obr{(sender as Karta).CisloObrazku}.png");//nastaví obrázek jako pozadí karty
+                var pozadi = new Bitmap((sender as Karta).BackgroundImage, new Size((sender as Karta).Width, (sender as Karta).Height));
+                (sender as Karta).BackgroundImage = pozadi;
+                (sender as Karta).Otocena = true;
+                if(otocenaKarta==0)//pokud je karta otočena první, tak si ji program zapamatuje
+                {
+                    otocenaKarta=(sender as Karta).CisloObrazku;
+                }
+                else
+                {
+                    if (otocenaKarta == (sender as Karta).CisloObrazku)//pokud se dvě otočené karty shodují, tak se zvýší skóre hráče a karty zmizí
+                    {
+                        hraci[index % (hraci.Count)].ziskaniBodu();
+                        muzeOtocit = false;
+                        await Task.Delay(1000);//čas na prohlédnutí karet
+                        muzeOtocit = true;
+                        for (int i = 0; i < 8; i++)
+                        {
+                            for (int j = 0; j < 8; j++)
+                            {
+                                if (pexeso[i, j].CisloObrazku == (sender as Karta).CisloObrazku)
+                                {
+                                    pexeso[i, j].Hide();
+                                }
+                            }
+                        }
+                        pocetDvojic--;
+                        if(pocetDvojic==0)
+                        {
+                            KonecHry();
+                        }
+                        
+                    }
+                    else//pokud se neshodují, tak se po jedné sekundě otočí zpátky, hraje další hráč
+                    {
+                        muzeOtocit = false;
+                        await Task.Delay(1000);//čas na prohlédnutí karet
+                        muzeOtocit = true;
+                        for (int i = 0; i < 8; i++)//otočí všechny otočené karty
+                        {
+                            for (int j = 0; j < 8; j++)
+                            {
+                                if (pexeso[i, j].Otocena)
+                                {
+                                    pexeso[i, j].BackgroundImage = Image.FromFile("Obrazky/PexesoRub.png");
+                                    pozadi = new Bitmap(pexeso[i, j].BackgroundImage, new Size(pexeso[i, j].Width, pexeso[i, j].Height));
+                                    pexeso[i, j].BackgroundImage = pozadi;
+                                    pexeso[i, j].Otocena = false;
+                                }
+                            }
+                        }
+                        DalsiHrac();
+                    }
+                    otocenaKarta = 0;
+                }
+            }
+        }
+        private void DalsiHrac()//zvýší index o jedna a zvýrázní hráče na tahu
+        {
+            hraci[index % (hraci.Count)].Jmeno.BackColor = Color.Empty;
+            index++;
+            hraci[index % (hraci.Count)].Jmeno.BackColor = Color.Red;
+        }
+        
+        private void KonecHry()//zobrazí pořadí hráčů a jejich skóre
+        {
+            hraci[index % (hraci.Count)].Jmeno.BackColor = Color.Empty;//zruší zvýraznění posledního hráče
+            hraci=hraci.OrderByDescending(hrac => hrac.skore).ToList();
+            for (int i = 0; i < hraci.Count; i++)
+            {
+                hraci[i].Jmeno.Location = new Point(900, 50 * i + 500);
+                hraci[i].Skore.Location = new Point(1020, 50 * i + 500);
+            }
+            konec.Show();
+        }
+    }
+}
